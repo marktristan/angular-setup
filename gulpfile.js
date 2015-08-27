79e773436982f98b22cd2bfab910fc039553a91b
Mark Tristan Victorio <m.victorio@arcanys.com>
2015-08-27 16:42:32 +0800

    Added gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var fs = require('fs');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+// var bower = require('gulp-bower');
+var run = require('gulp-run');
+
+gulp.task('setup', function () {
+    fs.mkdir('./public', function (err) {
+        if (!err) {
+            gutil.log('Creating application directories...');
+
+            fs.mkdir('public/src', function (err) {
+                fs.mkdir('public/src/config', function (err) {
+                    fs.writeFile('public/src/config/routes.js', '// Write your angular routes config here');
+                });
+
+                fs.mkdir('public/src/controllers');
+                fs.mkdir('public/src/directives');
+                fs.mkdir('public/src/factories');
+                fs.mkdir('public/src/filters');
+                fs.mkdir('public/src/services');
+
+                fs.writeFile('public/src/app.js', '// Write your main angular module here');
+            });
+
+            fs.mkdir('public/assets', function (err) {
+                fs.mkdir('public/assets/css');
+                fs.mkdir('public/assets/img');
+                fs.mkdir('public/assets/js');
+            });
+
+            fs.mkdir('public/views', function (err) {
+                fs.writeFile('public/views/index.html', '<!-- index.html -->');
+            });
+        }
+    });
+});
+ 
+gulp.task('bower', function () {
+    fs.stat('bower.json', function (err, stat) {
+        if(err == null) {
+            gutil.log('Installing dependencies...');
+        } else {
+            var bowerJsonText = "{\n\t\"name\": \"angular-setup\"\n}";
+
+            fs.writeFile('bower.json', bowerJsonText, function (err) {
+                if (err) throw err;
+                gutil.log('Created', '\'' + gutil.colors.magenta('bower.json') + '\'');
+                gutil.log('Installing dependencies...');
+            });
+        }
+        
+        run('bower install angular').exec()
+            .pipe(run('bower install bootstrap'));
+    });
+
+    // return bower();
+});
+
+gulp.task('init', ['setup', 'bower']);
